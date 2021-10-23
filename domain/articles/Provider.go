87579fd5909aa0200dc04763711c1710aa195f62
Josep Jesus Bigorra Algaba <josep@prowarehouse.nl>
2021-10-23 10:01:56 +0200

    add insert articles

--- domain/articles/Provider.go
+++ domain/articles/Provider.go
@@ -1,11 +1,39 @@
 package articles
 
+import (
+	"context"
+	"time"
+
+	"github.com/averageflow/joes-warehouse/infrastructure"
+)
+
 func GetArticlesForProduct() ([]ArticleModel, error) {
 	return nil, nil
 }
 
-func AddArticles(articles []ArticleModel) error {
-	return nil
+func AddArticles(db infrastructure.ApplicationDatabase, articles []ArticleModel) error {
+	ctx := context.Background()
+
+	tx, err := db.Begin(ctx)
+	if err != nil {
+		return err
+	}
+
+	now := time.Now().Unix()
+
+	for i := range articles {
+		if _, err := tx.Exec(
+			ctx,
+			addArticlesQuery,
+			articles[i].Name,
+			now,
+			now,
+		); err != nil {
+			return err
+		}
+	}
+
+	return tx.Commit(ctx)
 }
 
 func AddArticlesFromProductPayload(articles []LegacyArticleFromProductFileModel) error {
--- infrastructure/app/ArticleHandlers.go
+++ infrastructure/app/ArticleHandlers.go
@@ -1,13 +1,51 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"time"
+
+	"github.com/averageflow/joes-warehouse/domain/articles"
+	"github.com/averageflow/joes-warehouse/infrastructure"
+	"github.com/gin-gonic/gin"
+)
 
 func (s *ApplicationServer) getArticlesHandler() func(*gin.Context) {
 	return func(c *gin.Context) {}
 }
 
 func (s *ApplicationServer) addArticlesHandler() func(*gin.Context) {
-	return func(c *gin.Context) {}
+	type addArticlesRequest struct {
+		Data []articles.ArticleModel `json:"data"`
+	}
+
+	return func(c *gin.Context) {
+		var requestBody addArticlesRequest
+
+		if err := c.BindJSON(&requestBody); err != nil {
+			c.AbortWithStatusJSON(http.StatusUnprocessableEntity, ApplicationServerResponse{
+				Message:       infrastructure.GetMessageForHTTPStatus(http.StatusUnprocessableEntity),
+				Error:         err.Error(),
+				UnixTimestamp: time.Now().Unix(),
+			})
+
+			return
+		}
+
+		if err := articles.AddArticles(s.State.DB, requestBody.Data); err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, ApplicationServerResponse{
+				Message:       infrastructure.GetMessageForHTTPStatus(http.StatusInternalServerError),
+				Error:         err.Error(),
+				UnixTimestamp: time.Now().Unix(),
+			})
+
+			return
+		}
+
+		c.JSON(http.StatusOK, ApplicationServerResponse{
+			Message:       infrastructure.GetMessageForHTTPStatus(http.StatusOK),
+			UnixTimestamp: time.Now().Unix(),
+		})
+	}
 }
 
 func (s *ApplicationServer) addArticlesFromLegacyFileHandler() func(*gin.Context) {
