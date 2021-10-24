6fefb8604a4144292f14dffd6a0f106e668c18c9
Josep Jesus Bigorra Algaba <josep@prowarehouse.nl>
2021-10-24 14:26:42 +0200

    add transactions table

--- storage/migrations/1634917846_CreateDatabase.sql
+++ storage/migrations/1634917846_CreateDatabase.sql
@@ -38,3 +38,20 @@ CREATE TABLE IF NOT EXISTS product_articles (
     CONSTRAINT fk_product_articles_product FOREIGN KEY(product_id) REFERENCES products(id),
     CONSTRAINT fk_product_articles_article FOREIGN KEY(article_id) REFERENCES articles(id)
 );
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id SERIAL PRIMARY KEY,
+    unique_id uuid DEFAULT uuid_generate_v4(),
+    created_at BIGINT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS transaction_products (
+    id SERIAL PRIMARY KEY,
+    unique_id uuid DEFAULT uuid_generate_v4(),
+    transaction_id INT NOT NULL,
+    product_id INT NOT NULL,
+    amount_of BIGINT NOT NULL,
+    created_at BIGINT NOT NULL,
+    CONSTRAINT fk_transaction_products_transaction FOREIGN KEY(transaction_id) REFERENCES transactions(id),
+    CONSTRAINT fk_transaction_products_product FOREIGN KEY(product_id) REFERENCES products(id)
+);
